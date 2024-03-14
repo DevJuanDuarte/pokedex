a2976955b7a0dcbff586b5530b3b7cc8da5b3eb3
Juan Duarte <juan.devcontact@gmail.com>
2024-03-14 16:03:29 -0500

    Agregando un custom provider

--- README.md
+++ README.md
@@ -4,21 +4,19 @@
 
 # Ejecutar en Desarrollo
 1. Clonar el repositorio
+
 2. Ejecutar:
-```
-npm install
-```
+```npm install```
 
 3. Tener Nest Cli instalado
 
-```
-npm i -g @nestjs/cli
-```
+```npm i -g @nestjs/cli```
 
 4. Levantar la base de datos
-```
-docker-compose up -d
-```
+```docker-compose up -d```
+
+5. Reconstruir la base de datos con la semilla
+```localhost:3000/api/v2/seed```
 
 ## Stack Usado
 * MongoDB
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -5,6 +5,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
 import { Pokeroke } from './interfaces/pokemon.interface';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
@@ -12,21 +13,27 @@ export class SeedService {
   //3. Se llama al mismo constructor que tiene el PokemonService y se agregan sus importaciones
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http:AxiosAdapter
   ) { }
 
-  private readonly axios: AxiosInstance = axios;
+  
 
   async ejecutarSeed() {
 
+    //Elimina todos los registros de la coleccion antes de agregar los nuevos
     await this.pokemonModel.deleteMany({});
 
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1000')
+    //Se almacena en una constante la data desestructurada del endpoint y como es un generico la respuesta sera de tipo PokeResponse
+    const  data  = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1000')
 
     const pokemonToInsert:Pokeroke[] = [];
 
     data.results.forEach(async ({ name, url }) => {
+      
+      //Divide la url desde los valores que contengan '/'
       const segments = url.split('/');
+      
       const no = +segments[segments.length - 2]
       //4. Se agrega la constante pokemon que ejecuta desde el servicio el metodo create y colocamos la desestructuracion de name y no
       // const pokemon = await this.pokemonModel.create({ name, no });
